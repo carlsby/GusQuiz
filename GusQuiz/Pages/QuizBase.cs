ed39b2313ebde5659dea5fe7d3e4d26e6e8e13c6
carlsby <carlandersson99@outlook.com>
2023-01-24 21:01:05 +0100

    fixed playerwins count

--- GusQuiz/Pages/QuizBase.cs
+++ GusQuiz/Pages/QuizBase.cs
@@ -463,8 +463,232 @@ namespace GusQuiz.Pages
                 Answer = "Utvisning",
                 Category = "Fotboll"
             };
+            Question q38 = new Question
+            {
+                NameOfQuestion = "Nya testamentets fyra evangelister är Matteus, Markus, Lukas och...",
+                Options = new List<string>() { "Johannes", "Petrus", "Johan", "Peter" },
+                Answer = "Johannes",
+                Category = "Religionshistoria"
+            };
+            Question q39 = new Question
+            {
+                NameOfQuestion = "Vad heter den grekiska havsguden Poseidons motsvarighet i den romerska mytologin?",
+                Options = new List<string>() { "Jupiter", "Neptunus", "Bacchus", "Saturnus" },
+                Answer = "Neptunus",
+                Category = "Religionshistoria"
+            };
+            Question q40 = new Question
+            {
+                NameOfQuestion = "Oden är den högste av gudarna i den nordiska mytologin. Hugin och Munin är hans korpar. Men vad heter hans åttafotade häst?",
+                Options = new List<string>() { "Sleipner", "Rimfaxe", "Svadilfare", "Lokeis" },
+                Answer = "Sleipner",
+                Category = "Religionshistoria"
+            };
+            Question q41 = new Question
+            {
+                NameOfQuestion = "Johannes Döparens avrättning beskrivs i Nya testamentet. Hur dödas han?\r\n",
+                Options = new List<string>() { "Strypning", "Gift", "Dränkning", "Halshuggning" },
+                Answer = "Halshuggning",
+                Category = "Religionshistoria"
+            };
+            Question q42 = new Question
+            {
+                NameOfQuestion = "Vad tog Sverige avsked av 1866?",
+                Options = new List<string>() { "Ståndsriksdagen", "Lotteririksdagen", "Tvåkammarriksdagen", "Trekammarriksdagen" },
+                Answer = "Ståndsriksdagen",
+                Category = "Politisk historia"
+            };
+            Question q43 = new Question
+            {
+                NameOfQuestion = "År 1903 bildades föreningen LKPR. Vad krävde föreningen?",
+                Options = new List<string>() { "Kortare arbetstid", "Rösträtt för kvinnor", "Sänkt rösträttsålder", "Borttagning av dödsstraff" },
+                Answer = "Rösträtt för kvinnor",
+                Category = "Politisk historia"
+            };
+            Question q44 = new Question
+            {
+                NameOfQuestion = "För snart hundra år sedan, 1921, fick Sverige allmän och lika rösträtt. Varför behövde riksdagen fatta två beslut om detta, som dessutom var formulerade på exakt samma sätt?",
+                Options = new List<string>() { "Folket krävde detta", "Kungen krävde detta", "Eftersom det handlade om en ändring i grundlagen krävdes det två likadana beslut", "Ledamöterna i riksdagens första kammare hade bestämt att så skulle ske" },
+                Answer = "Eftersom det handlade om en ändring i grundlagen krävdes det två likadana beslut",
+                Category = "Politisk historia"
+            };
+            Question q45 = new Question
+            {
+                NameOfQuestion = "Efter valet 1921 tog fem kvinnor plats i riksdagen. Hur många procent av de som valdes in som riksdagsledamöter vid senaste valet var kvinnor?",
+                Options = new List<string>() { "33,7 procent", "67,2 procent", "46,1 procent", "49,4 procent" },
+                Answer = "46,1 procent",
+                Category = "Politisk historia"
+            };
+            Question q46 = new Question
+            {
+                NameOfQuestion = "Leonardo da Vinci målade Mona Lisa, eller La Gioconda, i början av 1500-talet. Var hänger den ikoniska tavlan i dag?",
+                Options = new List<string>() { "Louvren, Paris", "British Musem, London", "Houvren, Paris", "Paris Museum, Paris" },
+                Answer = "Louvren, Paris",
+                Category = "Konstens historia"
+            };
+            Question q47 = new Question
+            {
+                NameOfQuestion = "Vem komponerade De fyra årstiderna, på italienska Le quattro stagioni?",
+                Options = new List<string>() { "Giacomo Puccini", "Antonio Vivaldi", "Gioacchino Rossini", "Ludwig van Beethoven" },
+                Answer = "Antonio Vivaldi",
+                Category = "Konstens historia"
+            };
+            Question q48 = new Question
+            {
+                NameOfQuestion = "Den nederländske konstnären Vincent van Gogh tog sitt liv och blev berömd först efter sin död. Omtalat är också att han skar av en bit av en egen kroppsdel, vilken?",
+                Options = new List<string>() { "Pekfingret", "Överläppen", "Stortån", "Örat" },
+                Answer = "Örat",
+                Category = "Konstens historia"
+            };
+            Question q49 = new Question
+            {
+                NameOfQuestion = "Under vilket århundrade levde Carl Michael Bellman?",
+                Options = new List<string>() { "1500-talet", "1600-talet", "1700-talet", "1800-talet" },
+                Answer = "1700-talet",
+                Category = "Konstens historia"
+            };
+            Question q50 = new Question
+            {
+                NameOfQuestion = "Vilken händelse inleder andra världskriget 1 september 1939?",
+                Options = new List<string>() { "Tyskland invaderar Danmark", "Tyskland invaderar Polen", "Tyskland invaderar Sovjetunionen", "Sovjetunionen invaderar Estland" },
+                Answer = "Tyskland invaderar Polen",
+                Category = "Världskrig"
+            };
+            Question q51 = new Question
+            {
+                NameOfQuestion = "Våren 1940 ockuperade Tyskland två nordiska länder. Vilka länder var det?",
+                Options = new List<string>() { "Norge och Finland", "Danmark och Norge", "Sverige och Norge", "Danmark och Sverige" },
+                Answer = "Danmark och Norge",
+                Category = "Världskrig"
+            };
+            Question q52 = new Question
+            {
+                NameOfQuestion = "Vad hette mannen som sköt Franz Ferdinand?",
+                Options = new List<string>() { "Harald Pavić", "Harald Provice", "Gavrilo Princip", "Stanislav Princip" },
+                Answer = "Gavrilo Princip",
+                Category = "Världskrig"
+            };
+            Question q53 = new Question
+            {
+                NameOfQuestion = "Det serbiska terroristattentat som ledde fram till krigsutbrottet 1914 (och där den österrikisk-ungerske tronföljaren Franz Ferdinand och hans maka mördades) ägde rum i vilken stad?",
+                Options = new List<string>() { "Sarajevo", "Paris", "Hamburg", "Moskva" },
+                Answer = "Sarajevo",
+                Category = "Världskrig"
+            };
+            Question q54 = new Question
+            {
+                NameOfQuestion = "Vilken typ av glasyr går traditionellt på morotskaka?",
+                Options = new List<string>() { "Smörkräm", "Gräddfil", "Vispad grädde", "Färskost" },
+                Answer = "Färskost",
+                Category = "Desserter"
+            };
+            Question q55 = new Question
+            {
+                NameOfQuestion = "Vilket land kommer cheesecaken från?",
+                Options = new List<string>() { "Frankrike", "Finland", "England", "Italien" },
+                Answer = "Frankrike",
+                Category = "Desserter"
+            };
+            Question q56 = new Question
+            {
+                NameOfQuestion = "Vilken nöt döljer sig i julrisgrynsgröt i Sverige?",
+                Options = new List<string>() { "Jordnöt", "Hasselnöt", "Valnöt", "Mandel" },
+                Answer = "Mandel",
+                Category = "Desserter"
+            };
+            Question q57 = new Question
+            {
+                NameOfQuestion = "Vilken populär mexikansk dessert är täckt med karamell?",
+                Options = new List<string>() { "Shortcake", "Flan", "Cannoli", "Tiramisu" },
+                Answer = "Flan",
+                Category = "Desserter"
+            };
+            Question q58 = new Question
+            {
+                NameOfQuestion = "Vilken är den traditionella huvudefterrätten i det franska köket?",
+                Options = new List<string>() { "Kräm", "Ost", "Vispad grädde", "Frukt" },
+                Answer = "Ost",
+                Category = "Franska köket"
+            };
+            Question q59 = new Question
+            {
+                NameOfQuestion = "Vilken fransk delikatess är gjord på grund av vilken fransmän fick ett av deras smeknamn?",
+                Options = new List<string>() { "Spindlar", "Grodor", "Sniglar", "Gräshoppor" },
+                Answer = "Grodor",
+                Category = "Franska köket"
+            };
+            Question q60 = new Question
+            {
+                NameOfQuestion = "Vad heter traditionellt långt franskt bröd - mjukt inuti och med en krispig skorpa ute?",
+                Options = new List<string>() { "Baguette", "Calvados", "Shpalet", "Pain boule" },
+                Answer = "Baguette",
+                Category = "Franska köket"
+            };
+            Question q61 = new Question
+            {
+                NameOfQuestion = "Vad är bechamel?",
+                Options = new List<string>() { "En grundläggande sås", "En ostsort", "En frank efterrätt", "En fransk drink" },
+                Answer = "En grundläggande sås",
+                Category = "Franska köket"
+            };
+            Question q62 = new Question
+            {
+                NameOfQuestion = "Vilken stad dök den första italienska pizzan upp i?",
+                Options = new List<string>() { "Palermo", "Rom", "Neapel", "Milano" },
+                Answer = "Neapel",
+                Category = "Italienska köket"
+            };
+            Question q63 = new Question
+            {
+                NameOfQuestion = "Vilken av dessa är inte ingrediensen i carbonara?",
+                Options = new List<string>() { "Mjölk", "Ägg", "Bacon", "Ost" },
+                Answer = "Mjölk",
+                Category = "Italienska köket"
+            };
+            Question q64 = new Question
+            {
+                NameOfQuestion = "Vad är Calzone?",
+                Options = new List<string>() { "En grönsakssallad", "En vikad pizza", "En mängd pasta", "En pastasås" },
+                Answer = "En vikad pizza",
+                Category = "Italienska köket"
+            };
+            Question q65 = new Question
+            {
+                NameOfQuestion = "I Sverige har vi köttfärssåsen, men vad kallas denna förfinade rätt från Italien?",
+                Options = new List<string>() { "Pasta bolognese", "Pasta tomato", "Pasta salsiccio", "Pasta carbonara" },
+                Answer = "Pasta bolognese",
+                Category = "Italienska köket"
+            };
+            Question q66 = new Question
+            {
+                NameOfQuestion = "Vilken ört ingår i en Mojito?",
+                Options = new List<string>() { "Basilika", "Oregano", "Mynta", "Rosmarin" },
+                Answer = "Mynta",
+                Category = "Drinkar & cocktails"
+            };
+            Question q67 = new Question
+            {
+                NameOfQuestion = "Vilken juice är basen i en Bloody Mary?",
+                Options = new List<string>() { "Rödbetsjuice", "Apelsinjuice", "Tomatjuice", "Morotsjuice" },
+                Answer = "Tomatjuice",
+                Category = "Drinkar & cocktails"
+            };
+            Question q68 = new Question
+            {
+                NameOfQuestion = "Vilken drink förknippas med James Bond?",
+                Options = new List<string>() { "Dry Martini", "Negroni", "Whiskey Sour", "Old Fashioned" },
+                Answer = "Dry Martini",
+                Category = "Drinkar & cocktails"
+            };
+            Question q69 = new Question
+            {
+                NameOfQuestion = "Vad heter drinken med samma namn som en stadsdel i New York?",
+                Options = new List<string>() { "Queens", "Brooklyn", "Manhattan", "Bronx" },
+                Answer = "Manhattan",
+                Category = "Drinkar & cocktails"
+            };
 
-            Questions.AddRange(new List<Question> { q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20, q21, q22, q23, q24, q25, q26, q27, q28, q29, q30, q31, q32, q33, q34, q35, q36, q37 });
+            Questions.AddRange(new List<Question> { q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20, q21, q22, q23, q24, q25, q26, q27, q28, q29, q30, q31, q32, q33, q34, q35, q36, q37, q38, q39, q40, q41, q42, q43, q44, q45, q46, q47, q48, q49, q50, q51, q52, q53, q54, q55, q56, q57, q58, q59, q60, q61, q62, q63, q64, q65, q66, q67, q68, q69 });
         }
     }
 }
--- GusQuiz/Test/Class.cs
+++ GusQuiz/Test/Class.cs
@@ -1,29 +1,74 @@
-﻿using Xunit;
+﻿using GusQuiz.Components;
+using GusQuiz.Models;
+using Microsoft.Extensions.Options;
+using Xunit;
 
 namespace GusQuiz.Test
 {
     public class Class
     {
-
-        [Theory]
-        [InlineData(98, 5, 38)]
-        [InlineData(65, 5, 70)]
-        [InlineData(5, 5, 10)]
-
-        public void AdditionTest(int valueA, int valueB, int expected)
-
+        [Fact]
+        public void TestPlayer1Score()
         {
+            int player1Score = 20;
+            int player2Score = 10;
+            int player1Wins = 0;
 
-            // Arrange
-            var calculator = new Calculator();
+            Assert.True(player1Score > player2Score);
+        }
 
+        [Fact]
+        public void TestPlayer2Score()
+        {
+            int player1Score = 10;
+            int player2Score = 20;
 
-            // Act
-            var actual = calculator.Add(valueA, valueB);
+            Assert.True(player2Score > player1Score);
+        }
 
+        [Fact]
+        public void QuizTest()
+        {
+            string question1 = "test1";
+            string question2 = "test2";
+            string question3 = "test3";
+            string question4 = "test4";
+            string player1 = question1;
 
-            // Assert
-            Assert.Equal(expected, actual);
+            Assert.True(player1 == question1);
+        }
+
+        [Fact]
+        public void TestPlayer1Wins()
+        {
+            int player1Score = 10;
+            int player2Score = 5;
+            int player1Wins = 0;
+
+            if (player1Score > player2Score)
+            {
+                player1Wins++;
+            }
+
+            Assert.True(player1Score > player2Score);
+            Assert.Equal(1, player1Wins);
+        }
+
+        [Fact]
+        public void TestPlayer2Wins()
+        {
+            int player1Score = 5;
+            int player2Score = 10;
+            int player2Wins = 0;
+
+            if (player1Score < player2Score)
+            {
+                player2Wins++;
+            }
+
+            Assert.True(player1Score < player2Score);
+            Assert.Equal(1, player2Wins);
         }
     }
 }
+
